3e7413f03584cdd9de498d9c2da035909f84a4d9
I'm Naman Bansal <98607723+Namanb454@users.noreply.github.com>
2024-07-11 01:23:47 +0530

    Home Page UI

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,10 +1,27 @@
+import TotalBalanceBox from '@/components/TotalBalanceBox'
+import HeaderBox from '@/components/HeaderBox'
 import React from 'react'
 
 const Home = () => {
+  const loggedIn = { firsName: 'Naman' }
   return (
-    <div>
-      Home
-    </div>
+    <section className='home'>
+      <div className='home-content'>
+        <header className='home-header'>
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggedIn?.firsName || 'Guests'}
+            subtext='Access and manage your account and transcations efficiently.'
+          />
+          <TotalBalanceBox
+            accounts={[]}
+            totalBanks={1}
+            totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
   )
 }
 
